a5aef17af28283fa3a16f8edc872975d32ce62b6
Carolina <carolinaherreraglez2005@gmail.com>
2025-11-06 13:12:41 -0600

    svelte

---
+++ svelte-app/src/Hijo.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let color = 'blue';
+    export let texto = 'Boton';
+</script>
+<button style="background-color: {color};">
+     {texto}
+</button>
+
+<style>
+    button {
+        color: white;
+        border: none;
+        padding: 10px 20px;
+        font-size: 16px;
+        cursor: pointer;
+        border-radius: 5px;
+    }
+</style>
